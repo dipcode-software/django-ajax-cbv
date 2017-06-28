6ffdec1f4694aa8b962daa3b9357186abad02f58
Sandro Rodrigues <srtabs@gmail.com>
2017-06-28 11:14:32 +0100

    Added base ajax views

---
+++ ajax_views/views.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.views.generic import (
+    TemplateView, CreateView, UpdateView, DeleteView)
+
+from .mixins import PartialAjaxMixin, FormAjaxMixin, AjaxResponseMixin
+
+
+class TemplateAjaxView(PartialAjaxMixin, TemplateView):
+    """ """
+
+
+class CreateAjaxView(FormAjaxMixin, PartialAjaxMixin, CreateView):
+    """ """
+
+
+class UpdateAjaxView(FormAjaxMixin, PartialAjaxMixin, UpdateView):
+    """ """
+
+
+class DeleteAjaxView(PartialAjaxMixin, AjaxResponseMixin, DeleteView):
+    """ """
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return self.json_to_response()
